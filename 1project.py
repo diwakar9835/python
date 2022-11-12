37a582a1ae5dd2f0f572bf1d2e772c04c76db765
ankit diwakar <ankitdiwakar00000@gmail.com>
2022-11-12 16:08:09 +0530

    Rock Paper Scissors Game

---
+++ 1project.py
@@ -0,0 +1,7 @@
+#Rock Paper Scissors
+import random
+print("welcome to the RPS game")
+uC=input("Enter a either Rock Paper or Scissors")
+cI=["Rock","Paper","Scissors"] 
+cC=random.choice(cI)
+print(cC)
