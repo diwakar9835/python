e492fbad35dc0efb190f83ba2bba588d5f056aec
ankit diwakar <ankitdiwakar00000@gmail.com>
2022-11-13 21:58:20 +0530

    vovel/consonant

---
+++ vovel.py
@@ -0,0 +1,6 @@
+# Take the input character from the user and check if it is vovels or consonant
+ch=input("Enter the character to check if it is vovels or not ")
+if(ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u' or ch=='A' or ch=='E' or ch=='I' or ch=='O' or ch=='U'):
+    print("The character", ch, "is vovel")
+else:
+    print("It's consonant")
