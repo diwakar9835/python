6b66bedbca8cc0a9a7d3d3c4d4537aee6b81326b
ankit diwakar <ankitdiwakar00000@gmail.com>
2022-11-14 11:15:29 +0530

    using while loop display or print the value stored in the list

---
+++ listtype.py
@@ -0,0 +1,8 @@
+#using while loop display or print the values stored in the list
+list = [10,11,12,13,14]
+#print (len(list)) len---->length
+length=len(list) #5
+i=0 # initialization/start
+while i<length: # terminatuon condition/end/stopping condition
+    print(list[i])
+    i=i+1 #increment
