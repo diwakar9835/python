44211f68e97fb1c0e28ad91b215efb74dd560796
ankit diwakar <ankitdiwakar00000@gmail.com>
2022-11-18 19:41:43 +0530

    Finding Greatest number

---
+++ gratest.py
@@ -0,0 +1,10 @@
+print ("Enter first number")
+first = input()
+print ("Enter second number")
+second = input()
+if first>second:
+  print ("Greatest is",first)
+elif second>first:
+  print ("Greatest is",second)
+else:
+  print ("Both are equal")
