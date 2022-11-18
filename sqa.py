9bbe9d456d9e7ec3c19a814d13c25035d6676b5c
ankit diwakar <ankitdiwakar00000@gmail.com>
2022-11-18 19:37:59 +0530

    finding square / rectangle

---
+++ sqa.py
@@ -0,0 +1,8 @@
+print ("Enter length")
+length = input()
+print ("Enter breadth")
+breadth = input()
+if length == breadth:
+  print ("Yes, it is square")
+else:
+  print ("No, it is only Rectangle")
