4573f8f6ff0d367346ada575dd6a81e61c13dc16
ankit diwakar <ankitdiwakar00000@gmail.com>
2022-11-19 18:08:24 +0530

    grade making in result

---
+++ gra1.py
@@ -0,0 +1,11 @@
+per = int(input("Enter marks"))
+
+if per > 90:
+
+  print("Grade is A")
+
+if per > 80 and per <=90: print("Grade is B")
+
+if per >=60 and per <= 80: print("Grade is C")
+
+if per < 60: print("Grade is D")
