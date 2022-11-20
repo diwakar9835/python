290827ed8048d4b4ef4930ff52a4ee7ce5f6a5bf
ankit diwakar <ankitdiwakar00000@gmail.com>
2022-11-20 12:21:14 +0530

    mathematical operator

---
+++ ifelse6.py
@@ -0,0 +1,20 @@
+num1=int(input("Enter first number"))
+
+num2=int(input("Enter second number"))
+
+op=input("Enter mathematical operator")
+
+if op=='+':
+    print("Result is ", num1+num2)
+if op=='':
+    print("Result is ", num1-num2)
+if op=="*":
+    print("Result is ", num1*num2)
+if op=='/':
+    print("Result is ", num1/num2)
+if op=='%':
+    print("Result is ", num1% num2)
+if op=="**":
+    print("Result is ", num1**num2)
+if op=='//':
+    print("Result is", num1//num2)
