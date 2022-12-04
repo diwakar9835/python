8e897977e3c0635c7604a05c9f38c2e049e8d2d3
Ankit Diwakar <116998434+diwakar9835@users.noreply.github.com>
2022-12-04 15:45:50 +0530

    class creation and defination

---
+++ day33-1.py
@@ -0,0 +1,8 @@
+# class creation $ defination
+class Car:
+  height = 10
+
+tiago = Car() # object creation
+baleno = Car()
+print(tiago.height) #accessing the value in the class through object
+print(baleno.height)
