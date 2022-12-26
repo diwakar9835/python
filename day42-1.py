85d82dc4f78895e8ec8df8247186b7e71783dec3
Ankit Diwakar <116998434+diwakar9835@users.noreply.github.com>
2022-12-26 19:57:16 +0530

    Add files via upload

---
+++ day42-1.py
@@ -0,0 +1,9 @@
+pi = 3.142
+radius = 5
+def circle(n):
+    global radius
+    radius = radius*n
+    area_of_circle = pi*(radius)**2
+    print("{:.2f}".format(area_of_circle))
+n=int(input())
+circle(n)
